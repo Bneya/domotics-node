77daf54417847ba442069c43817f8c31fd3607f1
Benjamin <birivera@uc.cl>
2021-10-19 18:37:35 -0300

    ⏺ emiting deviceJoined

--- mqtt/topics/bridge-event.js
+++ mqtt/topics/bridge-event.js
@@ -1,5 +1,14 @@
+const websocket = require('../../websocket');
+
 const bridgeEvent = ({ msg }) => {
   console.log('device_announce', msg);
+
+  // Avisa al server si se conectó algo
+  if (msg.type === 'device_joined') {
+    const socketMsg = `Se unió el dispositivo ${msg.data.friendly_name}, ieee ${msg.data.ieee_address}`
+    console.log(socketMsg);
+    websocket.emit('deviceJoined', socketMsg);
+  }
 }
 
 module.exports = bridgeEvent;
